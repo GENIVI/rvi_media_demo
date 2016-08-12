ba3107af285b4089266295b55cb8add69a0f48c8
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-08-12 15:57:31 -0700

    [master] initial commit ... manually

---
+++ app/src/main/java/jlr/demo/media/rvi/rvi_media_demo/RviMediaDemo.java
@@ -0,0 +1,13 @@
+package jlr.demo.media.rvi.rvi_media_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RviMediaDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rvi_media_demo);
+    }
+}
