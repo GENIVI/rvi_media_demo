ed1417e51257a752561369f33059c89041a7331b
Aren Edlund <aedlund1@jaguarlandrover.com>
2016-08-22 15:11:21 -0700

    add rvi dependency

--- app/src/main/java/mediademo/jaguarlandrover/com/mediademo/MainActivity.java
+++ app/src/main/java/mediademo/jaguarlandrover/com/mediademo/MainActivity.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2016. Jaguar Land Rover.
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one
+ * at https://mozilla.org/MPL/2.0/.
+ * File: MainActivity.java
+ * Project: MediaDemo
+ * Created by aren on 8/22/16 3:09 PM
+ */
+
 package mediademo.jaguarlandrover.com.mediademo;
 
 import android.graphics.drawable.Drawable;
@@ -5,248 +15,26 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.SpinnerAdapter;
 
-/**
- * Created by aren on 8/22/16.
- */
-public class MainActivity extends ActionBar implements MediaManager.MediaManagerListener {
+import mediademo.jaguarlandrover.com.mediademo.MediaManager.MediaManagerListener;
+
+
+public class MainActivity extends AppCompatActivity implements MediaManagerListener {
     @Override
-    public void setCustomView(View view) {
+    public void onNodeConnected() {
 
     }
 
     @Override
-    public void setCustomView(View view, LayoutParams layoutParams) {
+    public void onNodeDisconnected() {
 
     }
 
     @Override
-    public void setCustomView(int resId) {
-
-    }
-
-    @Override
-    public void setIcon(@DrawableRes int resId) {
-
-    }
-
-    @Override
-    public void setIcon(Drawable icon) {
-
-    }
-
-    @Override
-    public void setLogo(@DrawableRes int resId) {
-
-    }
-
-    @Override
-    public void setLogo(Drawable logo) {
-
-    }
-
-    @Override
-    public void setListNavigationCallbacks(SpinnerAdapter adapter, OnNavigationListener callback) {
-
-    }
-
-    @Override
-    public void setSelectedNavigationItem(int position) {
-
-    }
-
-    @Override
-    public int getSelectedNavigationIndex() {
-        return 0;
-    }
-
-    @Override
-    public int getNavigationItemCount() {
-        return 0;
-    }
-
-    @Override
-    public void setTitle(CharSequence title) {
-
-    }
-
-    @Override
-    public void setTitle(@StringRes int resId) {
-
-    }
-
-    @Override
-    public void setSubtitle(CharSequence subtitle) {
-
-    }
-
-    @Override
-    public void setSubtitle(int resId) {
-
-    }
-
-    @Override
-    public void setDisplayOptions(int options) {
-
-    }
-
-    @Override
-    public void setDisplayOptions(int options, int mask) {
-
-    }
-
-    @Override
-    public void setDisplayUseLogoEnabled(boolean useLogo) {
-
-    }
-
-    @Override
-    public void setDisplayShowHomeEnabled(boolean showHome) {
-
-    }
-
-    @Override
-    public void setDisplayHomeAsUpEnabled(boolean showHomeAsUp) {
-
-    }
-
-    @Override
-    public void setDisplayShowTitleEnabled(boolean showTitle) {
-
-    }
-
-    @Override
-    public void setDisplayShowCustomEnabled(boolean showCustom) {
-
-    }
-
-    @Override
-    public void setBackgroundDrawable(@Nullable Drawable d) {
-
-    }
-
-    @Override
-    public View getCustomView() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public CharSequence getTitle() {
-        return null;
-    }
-
-    @Nullable
-    @Override
-    public CharSequence getSubtitle() {
-        return null;
-    }
-
-    @Override
-    public int getNavigationMode() {
-        return 0;
-    }
-
-    @Override
-    public void setNavigationMode(int mode) {
-
-    }
-
-    @Override
-    public int getDisplayOptions() {
-        return 0;
-    }
-
-    @Override
-    public Tab newTab() {
-        return null;
-    }
-
-    @Override
-    public void addTab(Tab tab) {
-
-    }
-
-    @Override
-    public void addTab(Tab tab, boolean setSelected) {
-
-    }
-
-    @Override
-    public void addTab(Tab tab, int position) {
-
-    }
-
-    @Override
-    public void addTab(Tab tab, int position, boolean setSelected) {
-
-    }
-
-    @Override
-    public void removeTab(Tab tab) {
-
-    }
-
-    @Override
-    public void removeTabAt(int position) {
-
-    }
-
-    @Override
-    public void removeAllTabs() {
-
-    }
-
-    @Override
-    public void selectTab(Tab tab) {
-
-    }
-
-    @Nullable
-    @Override
-    public Tab getSelectedTab() {
-        return null;
-    }
-
-    @Override
-    public Tab getTabAt(int index) {
-        return null;
-    }
-
-    @Override
-    public int getTabCount() {
-        return 0;
-    }
-
-    @Override
-    public int getHeight() {
-        return 0;
-    }
-
-    @Override
-    public void show() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public boolean isShowing() {
-        return false;
-    }
-
-    @Override
-    public void addOnMenuVisibilityListener(OnMenuVisibilityListener listener) {
-
-    }
-
-    @Override
-    public void removeOnMenuVisibilityListener(OnMenuVisibilityListener listener) {
+    public void onServiceInvoked(String serviceIdentifier, Object parameters) {
 
     }
 }
